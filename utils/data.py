35dd3a190d76db57460a3b2607818588ec9f2eb7
dmh1030 <71989584+dmh1030@users.noreply.github.com>
2021-06-08 01:46:44 -0500

    Add files via upload

--- utils/data.py
+++ utils/data.py
@@ -2,12 +2,15 @@ import pandas as pd
 import matplotlib.pyplot
 import numpy as np
 
+
 def load_csv_data(path_to_csv):
     df = pd.read_csv(path_to_csv)
     return df
 
+
 def filter_by_attribute(df, attribute, value):
     return df[df[attribute] == value]
+
 
 def get_cases_chronologically(df):
     cases = []
@@ -17,7 +20,21 @@ def get_cases_chronologically(df):
         _labels = df.iloc[i, :4]
         cases.append(_cases)
         labels.append(_labels)
-    
+
+    cases = np.array(cases)
+    labels = np.array(labels)
+    return cases, labels
+
+
+def get_cases_chronologically_state(df):
+    cases = []
+    labels = []
+    for i in range(df.shape[0]):
+        _cases = df.iloc[i, 11:]
+        _labels = df.iloc[i, 6]
+        cases.append(_cases)
+        labels.append(_labels)
+
     cases = np.array(cases)
     labels = np.array(labels)
     return cases, labels
